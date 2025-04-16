9bea95a6b809b202db5540669eecf8518a673d55
Makpal <maqpal.rus@gmail.com>
2025-04-17 03:20:02 +0500

    update

--- part1/SupportSystem.java
+++ part1/SupportSystem.java
@@ -1,4 +1,12 @@
 package part1;
 
 public class SupportSystem {
+    public static void main(String[] args) {
+        SupportHandler faq = new FAQBotHandler();
+
+        String[] issues = {"password_reset"};
+        for (String issue : issues) {
+            faq.handle(issue);
+        }
+    }
 }
